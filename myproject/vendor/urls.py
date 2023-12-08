1c50b16fff5a90d5f1e49da975e06e272d49d87c
Abhay <asingh20_be20@thapar.edu>
2023-12-09 01:26:37 +0530

    little work on serializers and views and urls of vendor app

---
+++ myproject/vendor/urls.py
@@ -0,0 +1,18 @@
+from django.urls import path
+from .views import (
+    vendor_list_create,
+    vendor_detail,
+    purchase_order_list_create,
+    purchase_order_detail,
+    vendor_performance,
+    acknowledge_purchase_order
+)
+
+urlpatterns = [
+    path('api/vendors/', vendor_list_create, name='vendor-list-create'),
+    path('api/vendors/<int:vendor_id>/', vendor_detail, name='vendor-detail'),
+    path('api/purchase_orders/', purchase_order_list_create, name='purchase-order-list-create'),
+    path('api/purchase_orders/<int:po_id>/', purchase_order_detail, name='purchase-order-detail'),
+    path('api/vendors/<int:vendor_id>/performance/', vendor_performance, name='vendor-performance'),
+    path('api/purchase_orders/<int:po_id>/acknowledge/', acknowledge_purchase_order, name='acknowledge-purchase-order'),
+]
--- myproject/vendor/views.py
+++ myproject/vendor/views.py
@@ -1,3 +1,93 @@
-from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from django.shortcuts import get_object_or_404
+from django.db.models import Avg, Count
+from datetime import timedelta
+from .models import Vendor, PurchaseOrder
+from .serializers import VendorSerializer, PurchaseOrderSerializer, VendorPerformanceSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def vendor_list_create(request):
+    if request.method == 'GET':
+        vendors = Vendor.objects.all()
+        serializer = VendorSerializer(vendors, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = VendorSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def vendor_detail(request, vendor_id):
+    vendor = get_object_or_404(Vendor, pk=vendor_id)
+
+    if request.method == 'GET':
+        serializer = VendorSerializer(vendor)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = VendorSerializer(vendor, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        vendor.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET', 'POST'])
+def purchase_order_list_create(request):
+    if request.method == 'GET':
+        purchase_orders = PurchaseOrder.objects.all()
+        serializer = PurchaseOrderSerializer(purchase_orders, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = PurchaseOrderSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def purchase_order_detail(request, po_id):
+    purchase_order = get_object_or_404(PurchaseOrder, pk=po_id)
+
+    if request.method == 'GET':
+        serializer = PurchaseOrderSerializer(purchase_order)
+        return Response(serializer.data)
+
+    elif request.method == 'PUT':
+        serializer = PurchaseOrderSerializer(purchase_order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        purchase_order.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+@api_view(['GET'])
+def vendor_performance(request, vendor_id):
+    vendor = get_object_or_404(Vendor, pk=vendor_id)
+    serializer = VendorPerformanceSerializer(vendor)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def acknowledge_purchase_order(request, po_id):
+    purchase_order = get_object_or_404(PurchaseOrder, pk=po_id)
+    
+    if request.method == 'POST':
+        serializer = PurchaseOrderSerializer(purchase_order, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            # Trigger recalculation logic for average_response_time
+            # You may implement the logic here or dispatch a task to a background job queue
+            return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
